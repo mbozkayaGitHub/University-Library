801cb3cd03b29189e55446e52bbb4c7d872499d3
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-10 15:25:36 +0300

    NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Nav,Logo,Menu, MenuLink, HamburgerIcon} from "./Navbar.style"
+import {menuIcon} from "../../helper/iconData"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+  const [showMenu, setShowMenu] = useState(false)
+  return (
+    <Nav justify="space-between" wrap="wrap" >
+      <Logo to="/">University Library</Logo>
+    <HamburgerIcon onClick={()=>setShowMenu(!showMenu)}>{menuIcon}</HamburgerIcon>
+    <Menu showMenu={showMenu}>
+      <MenuLink to="/">Home</MenuLink>
+      <MenuLink to="/about">About</MenuLink>
+      <MenuLink to="/register">Register</MenuLink>
+      <MenuLink to="/log in">Login</MenuLink>
+      <MenuLink to="/log out">Log out</MenuLink>
+    </Menu>
+    </Nav>
+  )
+};
+
+
+
+
+export default Navbar;
