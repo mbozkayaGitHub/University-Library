05348bf10fa48b778385437759380c526072fd79
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-10 16:21:29 +0300

    PrivateRouter

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -4,7 +4,7 @@ import { Navigate,Outlet } from 'react-router-dom';
 
 const PrivateRouter = () => {
   
-  const user = false;
+  const user = sessionStorage.getItem("user") || false ;
 
   return user ? <Outlet/> : <Navigate to="/login" />
 }
