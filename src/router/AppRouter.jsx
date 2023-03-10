679d25d09fe38cd6197db8c96b5c936c20b71f66
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-10 15:16:59 +0300

    AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import { BrowserRouter,Route,Roter,Routes } from 'react-router-dom'
-import Navbar from "../components/navbar/Navbar"
-import Footer from "../components/footer/Footer"
-import {GlobalStyles } from '../styles/Global.styles'
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import { GlobalStyles } from "../styles/Global.styles";
+import Footer from "../components/footer/Footer";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
-import Register from "../pages/register/Register"
+import Register from "../pages/register/Register";
+import PrivateRouter from "./PrivateRouter";
 import Detail from "../pages/detail/Detail";
 import About from "../pages/about/About";
-import PrivateRouter from './PrivateRouter';
 
 const AppRouter = () => {
-  return (                      
-   <BrowserRouter>
-   <Navbar/>
-   <GlobalStyles/>
-   <Routes>
-       <Route path="/" element={<Home/>} />
-       <Route path="/login" element={<Login/>} />
-       <Route path="/register" element={<Register/>} />
-       {/* <Route path="/about" element={<PrivateRouter/>}>
-       <Route path="" element={<About/>}/>
-       </Route>
-       <Route path="/detail" element={<PrivateRouter/>}>
-        <Route path="" element={<Detail/>}/>
-       </Route> */}
-
-       <Route element={<PrivateRouter/>}>
-       <Route path="/about" element={<About/>}>
-       <Route path="/detail" element={<Detail/>}>
+  const [currentUser, setCurrentUser] = useState(
+    sessionStorage.getItem("user")
+  );
+  return (
+    <BrowserRouter>
+      <Navbar currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login setCurrentUser={setCurrentUser} />}
+        />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
         </Route>
-   </Routes>
-   
-   <Footer/> 
-   </BrowserRouter>
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route> */}
+
+        <Route element={<PrivateRouter />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
-export default AppRouter:
+export default AppRouter;
