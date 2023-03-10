3775fe96ab9c48a614bae6993943aa53f38e6ab6
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-10 16:21:58 +0300

    NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 import {Nav,Logo,Menu, MenuLink, HamburgerIcon} from "./Navbar.style"
 import {menuIcon} from "../../helper/iconData"
 
-const Navbar = () => {
+const Navbar = ({currentUser,setCurrentUser}) => {
 
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const logout = () => {
+    setCurrentUser(false)
+    sessionStorage.clear();
+  }
   return (
     <Nav justify="space-between" wrap="wrap" >
       <Logo to="/">University Library</Logo>
@@ -12,9 +16,14 @@ const Navbar = () => {
     <Menu showMenu={showMenu}>
       <MenuLink to="/">Home</MenuLink>
       <MenuLink to="/about">About</MenuLink>
+      {currentUser ? ( 
+          <MenuLink to="/log out" onClick={logout}>Log out</MenuLink>
+      ) : (
+        <>
       <MenuLink to="/register">Register</MenuLink>
       <MenuLink to="/log in">Login</MenuLink>
-      <MenuLink to="/log out">Log out</MenuLink>
+      </>
+      )}
     </Menu>
     </Nav>
   )
