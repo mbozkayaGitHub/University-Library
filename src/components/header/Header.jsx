0bd34e226005069e5dd429cc05d188c942b33663
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-13 19:04:14 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import { HeaderButton, HeaderContainer, HeaderForm, HeaderTitle, SearchInput, SelectBox } from './Header.style';
+import { HeaderButton, HeaderContainer, HeaderForm, HeaderTitle, SearchInput, SelectBox } from './Header.style'
 
-const Header = () => {
+const Header = ({setQuery,setSelectType, getData}) => {
+  const printType = ["all", "books", "magazines"];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  }
+
   return (
- <HeaderContainer>
-  <HeaderTitle> Books or Magazine</HeaderTitle>
-  <HeaderForm>
-    <SearchInput type="text" placeholder="name" required />
-
-    <SelectBox>
-      <option value="">All</option>
-    </SelectBox>
-    <HeaderButton type="submit" >
-Search
-    </HeaderButton>
-  </HeaderForm>
- </HeaderContainer>
-
+    <HeaderContainer>
+      <HeaderTitle>Books or Magazines</HeaderTitle>
+      <HeaderForm onSubmit={handleSubmit}>
+        <SearchInput type="text" placeholder='name' onChange={(e)=> setQuery(e.target.value)} required/>
+        <SelectBox onChange={(e)=> setSelectType(e.target.value)}>
+          {
+            printType.map((type)=>{
+              return <option key={type} value={type}>{type}</option>;
+            })
+          }
+          </SelectBox> 
+          <HeaderButton type='submit'>
+            Search
+          </HeaderButton>
+      </HeaderForm>
+    </HeaderContainer>
   )
 }
 
-export default Header;
+export default Header
