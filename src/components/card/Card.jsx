9f133af92a679a13d5ed8e1effa0a21f776d1f4d
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-13 19:04:27 +0300

    Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
-const Card = () => {
+import { CardButton, CardContainer, CardHeader, CardImg } from './Card.style'
+import defaultImg from "../../assets/book.jpg"
+import { useNavigate } from 'react-router-dom'
+const Card = (item) => {
+  const navigate =useNavigate()
+  console.log(item);
   return (
-    <div>Card</div>
+  <CardContainer>
+    <CardHeader></CardHeader>
+    <CardImg src={item.volumeInfo.imageLinks.smallThumbnail || defaultImg}/>
+    <CardButton onClick={()=> navigate (`/detail/${item.id}`,{state:item})}>View More</CardButton>
+  </CardContainer>
   )
 }
 
